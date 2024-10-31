ede8a7739dd6d154c59e3d0024476b84b032ce82
munozDilean <dileanmunoz@outlook.com>
2024-10-31 13:56:26 -0400

    created primalfit db in container. Fixed url for backend to connect to the db.

--- primalFit/backend/app.py
+++ primalFit/backend/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 # from flask_cors import CORS
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://citizix_user:S3cret@db/citizix_db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@db/primalfit"
 db = SQLAlchemy(app)
 # CORS(app)
 
--- primalFit/backend/models.py
+++ primalFit/backend/models.py
@@ -4,6 +4,16 @@ from datetime import datetime
 
 # User Model
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+
+    def __init__(self, email, password):
+        self.email = email
+        self.password = password
+
+
 # Workout Model
 
 # Routine Model
