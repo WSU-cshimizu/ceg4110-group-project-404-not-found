8f224ffdf87a01adc90fa4b99b08e46f90f0f3a8
munozDilean <dileanmunoz@outlook.com>
2024-11-26 21:11:04 -0500

    added food page with components.

---
+++ primalFit/frontend/src/hooks/useFetch.jsx
@@ -0,0 +1,34 @@
+import Papa from 'papaparse'
+
+const useFetch = () => {
+
+    const sanitizeColumns = (data) => {
+        return data.map((item) => {
+            const sanitizedItem = {};
+            Object.keys(item).forEach(key => {
+                const sanitizedKey = key.toLowerCase();
+                sanitizedItem[sanitizedKey] = item[key];
+            });
+            return sanitizedItem;
+        });
+    }
+
+    const fetchCsvData = async (filePath, voidCallback) => {
+
+        const response = await fetch(filePath);
+        const reader = response.body.getReader();
+        const result = await reader.read();
+        const decoder = new TextDecoder('utf-8');
+        const csvString = decoder.decode(result.value);
+        const { data } = Papa.parse(csvString, {
+            header: true,
+            dynamicTyping: true
+        })
+
+        const sanitizedData = sanitizeColumns(data);
+        voidCallback(sanitizedData);
+    }
+    return { fetchCsvData }
+}
+
+export default useFetch;
---
+++ primalFit/frontend/src/pages/FoodPage.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import SearchBar from '../components/SearchBar'
+
+import useFetch from "../hooks/useFetch";
+import FoodTable from "../components/FoodTable";
+
+export default function FoodPage() {
+  const [foodList, setFoodList] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const { fetchCsvData } = useFetch();
+
+  const search = (data) => {
+    return data.filter((item) => {
+      if (item.food) {
+        return item.food.toLowerCase().includes(query);
+      }
+      else return false;
+    })
+  };
+
+  // Get food List on load
+  useEffect(() => {
+    fetchCsvData("/data/nutrients.csv", setFoodList);
+  }, [])
+
+  return (
+    <div>
+      <h1 className="text-center text-2xl mb-2">Food Search</h1>
+      <SearchBar onChange={(q) => setQuery(q)} />
+      <FoodTable foodList={search(foodList)} />
+    </div>
+  );
+}
