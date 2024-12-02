2a115d5735a96bcd653abe831382c303c530cc2b
Aziz Saleh <saleh.58@wright.edu>
2024-12-01 23:56:00 -0500

    updated account page it needs to be tested with api and json data and currently trying make the values dynamic in daily nutrition

--- primalFit/frontend/src/components/FoodTable.jsx
+++ primalFit/frontend/src/components/FoodTable.jsx
@@ -1,31 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 
 const controller = require("../Controller.jsx")
 
 const FoodTable = (props) => {
 
     const foodList = props.foodList;
-    const mealType = props.mealType;
 
-    const [message, setMessage] = useState({ success: false, message: "" });
-
-    const addFoodToUser = async (e, food) => {
+    const addFoodToUser = async (e, food, mealType) => {
         const { id } = await controller.login("test3@test3.com", "password3");
-        controller.addFood(id, food.food, food.calories, food.protein, food.carbs, food.fat, mealType)
-            .then((resJson) => {
-                console.log(resJson);
-                setMessage({ success: true, message: `Food ${food.food} added to user successfully` })
-
-                setTimeout(() => {
-                    setMessage({ success: false, message: "" });
-                }, 2000)
-            }).catch((err) => {
-                setMessage({ success: false, message: "Error occurred while adding the food" })
-
-                setTimeout(() => {
-                    setMessage({ success: false, message: "" });
-                }, 2000)
-            });
+        const resJson = await controller.addFood(id, food.food, food.calories, food.protein, food.carbs, food.fat, mealType)
+        console.log(id, resJson);
     }
 
     // Examples 
@@ -64,39 +48,38 @@ const FoodTable = (props) => {
     // }
 
     return (
-        <div>
-            <p className={message.success ? "text-green-500" : "text-red-500"}>{message.message ? message.message : ''}</p>
-            <table className="w-full text-center">
+        <table className="w-full text-center">
+            <tr>
+                <th>Food</th>
+                <th>Grams</th>
+                <th>Calories</th>
+                <th>Proteins</th>
+                <th>Fat</th>
+                <th>Carbs</th>
+                <th>Category</th>
+                <th>Add Food</th>
+            </tr>
+            {foodList.map(food =>
+            (
                 <tr>
-                    <th>Food</th>
-                    <th>Calories</th>
-                    <th>Proteins</th>
-                    <th>Fat</th>
-                    <th>Carbs</th>
-                    <th>Category</th>
-                    <th>Add Food</th>
+                    <td>{food.food}</td>
+                    <td>{food.grams}</td>
+                    <td>{food.calories}</td>
+                    <td>{food.protein}</td>
+                    <td>{food.fat}</td>
+                    <td>{food.carbs}</td>
+                    <td>{food.category}</td>
+                    <td className="flex items-center justify-center">
+                        <button onClick={(e) => addFoodToUser(e, food, "breakfast")} className="flex justify-center items-center select-none">
+                            <svg className="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5" />
+                            </svg>
+                        </button>
+                    </td>
                 </tr>
-                {foodList.map(food =>
-                (
-                    <tr>
-                        <td>{food.food}</td>
-                        <td>{food.calories}</td>
-                        <td>{food.protein}</td>
-                        <td>{food.fat}</td>
-                        <td>{food.carbs}</td>
-                        <td>{food.category}</td>
-                        <td className="flex items-center justify-center">
-                            <button onClick={(e) => addFoodToUser(e, food, "breakfast")} className="flex justify-center items-center select-none">
-                                <svg className="w-6 h-6 text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                                    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5" />
-                                </svg>
-                            </button>
-                        </td>
-                    </tr>
-                )
-                )}
-            </table>
-        </div>
+            )
+            )}
+        </table>
     )
 }
 
