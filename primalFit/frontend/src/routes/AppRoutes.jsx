bb9c7e612ad167d608bc397242be48c0000df87f
munozDilean <dileanmunoz@outlook.com>
2024-12-02 11:33:54 -0500

    Started merging aziz changes with repo.
    Co-authored-by: aziz7154 <aziz7154@users.noreply.github.com>

--- primalFit/frontend/src/routes/AppRoutes.jsx
+++ primalFit/frontend/src/routes/AppRoutes.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard.jsx";
-import AccountPage from "./pages/AccountPage.jsx";
-import Daily_Nutrition from "./pages/Daily_Nutrition.jsx";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "../pages/Dashboard.jsx";
+import AccountPage from "../pages/AccountPage.jsx";
+import Daily_Nutrition from "../pages/Daily_Nutrition.jsx";
+import FoodPage from "../pages/FoodPage.jsx";
 
 function AppRoutes() {
   return (
-    <Router>
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/account" element={<AccountPage />} />
         <Route path="/nutrition" element={<Daily_Nutrition/>} />
         <Route path="/routines" element={<div>Routines Page</div>} />
-        <Route path="/food-search" element={<div>Food Search Page</div>} />
+        <Route path="/food" element={<FoodPage/>} />
       </Routes>
-    </Router>
   );
 }
 
